e3e04cc426ddf5204c9c03d0fd0b9cb860464a9f
ordep96 <pedro.pmo55@gmail.com>
2018-08-22 16:47:21 -0400

    Vista Login

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { View, Image, Text } from 'react-native';
-import { Button, Container, Content, Form, Item, Input, Label } from 'native-base';
+import { Button, Container, Form, Item, Input, Label } from 'native-base';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 import styles from '../styles/loginStyles'
 import { connect } from 'react-redux';
 import { login } from '../actions/index';
@@ -10,12 +11,12 @@ class LoginScreen extends Component {
 static navigationOptions = {
     header: null
 };
- 
+
 constructor(props) {
     super(props);
     //States
-    this.state = { 
-        email: '', 
+    this.state = {
+        email: '',
         password: ''
     };
     //Functions
@@ -46,36 +47,57 @@ onSubmit() {
 render() {
 
     return (
-        <Container style={styles.containerLogin}>
-           <Image style={styles.imprimirLogo} source={require('../assets/images/imprimir-png-verde.png')} /> 
-           <Content style={styles.loginForm}>
-                <Form> 
-                    <Item floatingLabel>
-                    <Label>Email</Label>
-                    <Input value={this.state.email} onChange={this.onChange.bind(this, 'email')}/>
-                    </Item>
-                    <Item floatingLabel last>
-                    <Label>Contraseña</Label>
-                    <Input secureTextEntry={true} value={this.state.password} onChange={this.onChange.bind(this, 'password')}/>
-                    </Item>
-                </Form>
-                <View style={styles.buttonLoginContainer}>
-                    <Button style={styles.buttonLogin} onPress={this.onSubmit}>
-                        <Text style={styles.buttonLoginText}>Entrar</Text>
-                    </Button>
-                </View>
-            </Content>
+    <Container style={styles.containerLogin}>
+      <Image
+        style={styles.imprimirLogo}
+        source={require('../assets/images/imprimir-png-verde.png')}
+      />
+      <View style={styles.wrapperLogin}>
+
+        <View style={styles.loginForm}>
+            <Form style={{width:300}}>
+              <View style={styles.field}>
+                <Icon  name="user" style={styles.iconSignup}/>
+                <Input
+                  value={this.state.email}
+                  onChange={this.onChange.bind(this, 'email')}
+                  placeholder="Usuario / Email"
+                />
+              </View>
+              <View style={styles.field}>
+                <Icon name="lock" style={styles.iconSignup} />
+                <Input
+                  secureTextEntry={true}
+                  value={this.state.password}
+                  onChange={this.onChange.bind(this, 'password')}
+                  placeholder="Contraseña"
+                />
+              </View>
+            </Form>
             <View style={styles.allButtonsTransparentContainer}>
-                <Button transparent style={styles.buttonTransparent} onPress={() => this.props.navigation.push('PasswordRecovery')}>
-                    <Text style={styles.buttonRecoveryText}>¿Olvidaste tu contraseña? Haz clic aquí</Text>
+                <Button transparent style={styles.buttonTransparent} onPress={() => this.props.navigation.navigate('PasswordRecovery')}>
+                    <Text style={styles.buttonRecoveryText}>
+                      ¿Olvidaste tu contraseña?
+                    </Text>
                 </Button>
             </View>
-            <View style={styles.allButtonsTransparentContainer}>
-                <Button transparent style={styles.buttonTransparent} onPress={() => this.props.navigation.push('Presignup')}>
-                    <Text style={styles.buttonSignupText}>¿Aún no tienes una cuenta? Regístrate aquí</Text>
+
+            <View style={styles.buttonLoginContainer}>
+                <Button style={styles.buttonLogin} onPress={this.onSubmit}>
+                    <Text style={styles.buttonLoginText}>Entrar</Text>
                 </Button>
             </View>
-        </Container>
+        </View>
+
+
+        <View style={styles.allButtonsTransparentContainer}>
+            <Button transparent style={styles.buttonTransparent} onPress={() => this.props.navigation.push('Presignup')}>
+                <Text style={styles.buttonSignupText}>¿Aún no tienes una cuenta? Regístrate aquí</Text>
+            </Button>
+        </View>
+
+      </View>
+    </Container>
 
     );
   }
@@ -85,5 +107,5 @@ const mapStateToProps = (state) => ({
     success: state.success,
     error: state.error
 });
-  
+
 export default connect(mapStateToProps)(LoginScreen);
