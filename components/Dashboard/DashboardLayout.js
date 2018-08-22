bc7f7c6bb380cca7990c2b6f388bf80c228d4cd9
ordep96 <pedro.pmo55@gmail.com>
2018-08-22 16:48:36 -0400

    Agregada animacion  a la hora de abrir el menu

--- components/Dashboard/DashboardLayout.js
+++ components/Dashboard/DashboardLayout.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import { Animated, View } from 'react-native';
+import { Animated, View, Easing } from 'react-native';
 import { Container, Drawer } from 'native-base';
+
+
 
 import HeaderDashboard from './Header';
 import Menu from './Menu';
+
+import { styles, drawerStyles } from '../../styles/drawerStyles';
+
 
 export default class DashboardLayout extends Component {
 
@@ -13,37 +18,73 @@ export default class DashboardLayout extends Component {
   };
 
 
+
   state = {
     left:new Animated.Value(-1000),
-    openMenu:false
+    contentToLeft:new Animated.Value(0)
   }
 
   openMenu = () => {
-      this.drawer._root.open()
+    this.drawer._root.open()
   }
 
   closeMenu = () => {
     this.drawer._root.close()
+  }
+
+  handleContentToLeft = () => {
+
+    Animated.timing(
+      this.state.contentToLeft,
+      {
+        toValue:400,
+        easing:Easing.in(),
+        duration:250
+      }
+    ).start()
+  }
+
+  handleCloseContentToLeft = () => {
+    Animated.timing(
+      this.state.contentToLeft,
+      {
+        toValue:0,
+        easing:Easing.in(),
+        duration:250
+      }
+    ).start()
 
   }
 
-
   render(){
     return (
-      <Drawer
-        ref={(ref) => {this.drawer = ref;}}
-        content={<Menu navigator={this.navigator} go={this.props.children.props.go} />}
-        onClose={() => this.closeMenu()}
-      >
-        <Container style={this.props.style}>
-          <HeaderDashboard
-              onPress={this.state.openMenu ? this.closeMenu : this.openMenu}
-              titleHeader={this.props.titleHeader}
-            />
+        <Drawer
+          ref={(ref) => {this.drawer = ref;}}
+          content={<Menu navigator={this.navigator} go={this.props.children.props.go} />}
+          onClose={() => this.closeMenu()}
+          styles={drawerStyles}
+          onOpenStart={this.handleContentToLeft}
+          onCloseStart={this.handleCloseContentToLeft}
+          tweenDuration={250}
+        >
+          <Container style={this.props.style}>
+            <HeaderDashboard
+                onPress={this.openMenu}
+                titleHeader={this.props.titleHeader}
+              />
 
-            { this.props.children }
-        </Container>
-      </Drawer>
+            <Animated.View
+              style={{
+                flex:1,
+                position:'relative',
+                left:this.state.contentToLeft
+              }}
+            >
+              {this.props.children}
+            </Animated.View>
+          </Container>
+        </Drawer>
+
     )
   }
 
